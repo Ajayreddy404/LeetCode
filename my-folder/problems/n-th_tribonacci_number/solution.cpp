773f49cd29275685c01755056f9ab847ab625e1f
Ajay Reddy <87086062+Ajayreddy404@users.noreply.github.com>
2022-07-20 10:30:55 +0000

    Sync LeetCode submission - N-th Tribonacci Number (cpp)

--- my-folder/problems/n-th_tribonacci_number/solution.cpp
+++ my-folder/problems/n-th_tribonacci_number/solution.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        int num0 = 0, num1 = 1, num2 = 1;
-        if(n==0){
-            return num0;
-        }
-        
+        int x=0, y=1, z=1;
+        if(n==0)    return 0;
         for(int i=3; i<=n; i++){
-            int temp = num0 + num1 + num2;
-            num0 = num1;
-            num1 = num2;
-            num2 = temp;
+            int temp = x+y+z;
+            x = y;  y = z;  z = temp;
         }
-        return num2;
+        return z;
     }
 };
