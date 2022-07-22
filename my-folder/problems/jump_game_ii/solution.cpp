5d3a01bf1f5dc4139465d644bd46d80c8f2d20b2
Ajay Reddy <87086062+Ajayreddy404@users.noreply.github.com>
2022-07-22 06:01:48 +0000

    Sync LeetCode submission - Jump Game II (cpp)

--- my-folder/problems/jump_game_ii/solution.cpp
+++ my-folder/problems/jump_game_ii/solution.cpp
@@ -1,30 +1,21 @@
-void solve(vector<int> &nums, vector<int> &dp, int i, int &n){
-    if(i>=n){
-        return;
-    }
-    //if(dp[i]!= -1){
-    //    return dp[i];
-    //}
-    int j;
-    for(j=1; j<=nums[i] && i+j<n; j++){
-        dp[i+j] = min(dp[i+j], 1+dp[i]);
-        //cout<<i+j<<" : "<<dp[i+j]<<endl;
-    }
-    solve(nums,dp,i+1, n);
-    
-    
-}
-
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1){
-            return 0;
+    int n;
+    int dp[100100];
+    
+    int rec(int level, vector<int>& arr){
+        if(level>=n-1)  return 0;
+        if(dp[level]!=-1)   return dp[level];
+        int ans = 1e5;
+        for(int i=arr[level]; i>0; i--){
+            ans = min(ans, 1+rec(level+i,arr));
         }
-        vector<int> dp(n+1,100000);
-        dp[0] = 0;
-        solve(nums, dp, 0, n);
-        return dp[n-1];
+        return dp[level] = ans;
+    }
+    
+    int jump(vector<int>& nums) {
+        n = nums.size();
+        memset(dp,-1,sizeof(dp));
+        return rec(0,nums);
     }
 };
