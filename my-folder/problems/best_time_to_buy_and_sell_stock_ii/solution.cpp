d9ec6c1f8b9f11b5786456aca83495aa62512c92
Ajay Reddy <87086062+Ajayreddy404@users.noreply.github.com>
2022-07-26 04:21:03 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (cpp)

--- my-folder/problems/best_time_to_buy_and_sell_stock_ii/solution.cpp
+++ my-folder/problems/best_time_to_buy_and_sell_stock_ii/solution.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        
-        int profit = 0;
-        
-        for(int i = 0; (i+1)<prices.size(); ){
-            
-            int b_count = 0, buy = prices[i];
-            while(b_count==0 && (i+1)<prices.size()){
-                if(prices[i+1] > buy){
-                    b_count++;
-                    i++;
-                }
-                else{
-                    buy = prices[i+1];
-                    i++;
-                }
+    int maxProfit(vector<int>& arr) {
+        // sum(local max - just prev local min)
+        int ans = 0, n = arr.size();
+        for(int i=0; i<n; ){
+            int minv = 1e5, maxv = -1;
+            while(i<n && minv>arr[i]){
+                minv = arr[i];
+                i++;
+            }
+            while(i<n && maxv<arr[i]){
+                maxv = arr[i];
+                i++;
             }
             
-            int s_count = 0, sell = prices[i];
-            while(s_count==0 && (i+1)<prices.size()){
-                if(prices[i+1] < sell){
-                    s_count++;
-                    i++;
-                }
-                else{
-                    sell = prices[i+1];
-                    i++;
-                }
-            }
-            
-            profit += sell - buy;
+            if(maxv!=-1)    ans += abs(maxv-minv);
         }
-        return profit;
+        return ans;
     }
 };
