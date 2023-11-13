7f440e3da6c9db8bae8735205b4e868cbc5baff9
Ajay Reddy <87086062+Ajayreddy404@users.noreply.github.com>
2023-11-13 16:34:00 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- my-folder/problems/two_sum/solution.cpp
+++ my-folder/problems/two_sum/solution.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> op(2);
-        
-        for(int i=0; i<nums.size(); i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i]+nums[j] == target){
-                    op[0] = i;
-                    op[1] = j;
-                    break;
-                }
+    vector<int> twoSum(vector<int>& num, int target) {
+        int n = num.size();
+
+        // noting indices and elements in a seperate vector
+        vector<pair<int,int>> nums;
+        for(int i=0; i<n; i++){
+            nums.push_back(make_pair(num[i],i));
+        }
+
+        // sorting according to the first element
+        sort(nums.begin(),nums.end());
+
+        // finding the pair using two pointer approach
+        for(int i=0, j=n-1; i<j; ){
+            // cout<<i<<j<<nums[i].first<<nums[j].first<<endl;
+            if(nums[i].first+nums[j].first==target){
+                cout<<nums[i].first<<" "<<nums[j].first<<endl;
+                return {nums[i].second,nums[j].second};
+            }else if(nums[i].first+nums[j].first>target){
+                // cout<<"here"<<endl;
+                j--;
+            }else{
+                i++;
             }
         }
-        
-        return op;
+        return {1,1};
     }
 };
