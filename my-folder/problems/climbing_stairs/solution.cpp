131e5cf8afa59cca8fb32a4115ff063db1b2ffda
Ajay Reddy <87086062+Ajayreddy404@users.noreply.github.com>
2023-11-26 01:06:43 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

--- my-folder/problems/climbing_stairs/solution.cpp
+++ my-folder/problems/climbing_stairs/solution.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    
     int N;
     int dp[50];
     
     int rec(int level){
         // base case and pruning
-        if(level > N){
-            return 0;
-        }
-        if(level==N){
-            return 1;
-        }
-        
+        if(level > N)   return 0;
+        if(level == N)  return 1;
+
         // cache check
         if(dp[level]!=-1)   return dp[level];
-        
-        // transit and calc
-        int ans = rec(level+1) + rec(level+2);
-        
+
+        // transition
+        int ways = rec(level+1) + rec(level+2);
+
         // save and return
-        return dp[level] = ans;
+        return dp[level] = ways;
     }
-    
+
     int climbStairs(int n) {
         N = n;
         memset(dp,-1,sizeof(dp));
