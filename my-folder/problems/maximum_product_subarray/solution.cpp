b60a757964eea0b35b0a3cbaaca4a617469069af
Ajay Reddy <87086062+Ajayreddy404@users.noreply.github.com>
2023-11-30 16:29:30 +0000

    Sync LeetCode submission - Maximum Product Subarray (cpp)

--- my-folder/problems/maximum_product_subarray/solution.cpp
+++ my-folder/problems/maximum_product_subarray/solution.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
     int maxProduct(vector<int>& arr) {
-        int n = arr.size();
-        if(n==1)    return arr[0];
-        bool zero = false;
-        int max_len = -10;
-        for(int j=0; j<n; j++){
-            int f0 = j-1, s0 = n;
-            for(int i=j; i<n; i++){
-                if(arr[i]==0){
-                    zero = true;
-                    s0 = i;
-                    break;
-                }
-            }j = s0;
-            if(s0-f0==1)    continue;
-            // cout<<f0<<" "<<s0<<endl;
-            int fnp = 1, lnp = 1, nc = 0, cur_prod=1;
-            for(int i=f0+1; i<s0; i++){
-                fnp*=arr[i];
-                if(arr[i]<0)    break;
-            }
-            for(int i=s0-1; i>f0; i--){
-                lnp*=arr[i];
-                if(arr[i]<0)    break;
-            }
-            for(int i=f0+1; i<s0; i++){
-                if(arr[i]<0)    nc++;
-                cur_prod *= arr[i];
-            }
+        int mp = -1e9, cp = 1, cnt = 0, n = arr.size();
 
-            if(nc%2==1 && s0-f0>2){
-                cur_prod = max(cur_prod/fnp, cur_prod/lnp);
+        for(int i=0; i<n; i++){
+            if(arr[i]==0){
+                cp = 1; continue;
+            }else{
+                cp *= arr[i];
+                mp = max(cp, mp);
             }
-            // cout<<fnp<<" "<<lnp<<" "<<cur_prod<<endl;
-            max_len = max(cur_prod,max_len);
         }
-        if(zero && max_len<0)   return 0;
-        return max_len;
         
+        cp = 1;
+
+        for(int i=n-1; i>=0; i--){
+            if(arr[i]==0){
+                cp = 1;
+                cnt++;
+                continue;
+            }else{
+                cp *= arr[i];
+                mp = max(cp, mp);
+            }
+        }
+
+        if(mp<0 && cnt>0)   mp = 0;
+
+        return mp;
     }
 };
