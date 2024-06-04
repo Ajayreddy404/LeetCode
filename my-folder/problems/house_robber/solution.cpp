93c417da6f6d6c3f386d3ab3eaf0b8a0c523e4a9
Ajay Reddy <87086062+Ajayreddy404@users.noreply.github.com>
2024-06-04 17:59:29 +0000

    Sync LeetCode submission - House Robber (cpp)

--- my-folder/problems/house_robber/solution.cpp
+++ my-folder/problems/house_robber/solution.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    int n;
-    // int arr;
-    int dp[101];
-    
-    int rec(int level, vector<int>& arr){
+    int n, dp[101];
+    int rec(int level, vector<int>& nums){
         if(level>=n)    return 0;
         if(dp[level]!=-1)   return dp[level];
-        int ans = max(arr[level]+rec(level+2,arr), rec(level+1,arr));
+        int ans = max(nums[level] + rec(level+2,nums), rec(level+1,nums));
         return dp[level] = ans;
     }
-    
+
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
         n = nums.size();
+        if(n==1)    return nums[0];
+        memset(dp,-1,sizeof(dp));
         int ans = max(rec(0,nums), rec(1,nums));
         return ans;
     }
