4d8734ebcf091afdf97b5cb49e81ee70d00073a8
Ajay Reddy <87086062+Ajayreddy404@users.noreply.github.com>
2024-08-20 02:36:38 +0000

    Sync LeetCode submission - 3Sum (cpp)

--- my-folder/problems/3sum/solution.cpp
+++ my-folder/problems/3sum/solution.cpp
@@ -1,33 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-
         int n = nums.size();
+        sort(nums.begin(),nums.end());
+        // for(auto x: nums){
+        //     cout<<x<<" ";
+        // }cout<<endl;
+        set<vector<int>> st;
+        vector<vector<int>> vec;
 
         for(int i=0; i<n; i++){
-            int j=i+1, k = n-1;
+            // int i1 = i+1;
+            // cout<<i<<endl;
+            // while(i1<n && nums[i1]==nums[i]){
+            //     i1++; i++;
+            // }
+            
+            int j = i+1, k = n-1;
+            // cout<<i<<j<<k<<endl;
+            
             while(j<k){
+                // cout<<i<<j<<k<<endl;
                 int csum = nums[i]+nums[j]+nums[k];
-                if(csum>0){
+                if(csum==0){
+                    st.insert({nums[i],nums[j],nums[k]});
+                    j++; k--;
+                }else if(csum>0){
+                    // int k1 = k;
+                    // while(k>j && nums[k1]==nums[k])    
                     k--;
-                }else if(csum<0){
+                }else{
+                    // int j1 = j;
+                    // while(j<k && nums[j1]==nums[j])    
                     j++;
-                }else if(csum==0){
-                    // cout<<i<<" "<<j<<" "<<k<<endl;
-                    ans.push_back({nums[i],nums[j],nums[k]});
-                    int j1 = j, k1 = k;
-                    // removing duplicates as 2nd and 3rd element
-                    while(j<k && nums[j]==nums[j1])  j++;
-                    while(j<k && nums[k]==nums[k1])  k--;
                 }
-                
             }
-
-            // removing duplicated at 1st element
-            while(i+1<n && nums[i]==nums[i+1])  i++;
+            
         }
-        return ans;
+
+        for(auto v: st){
+            vec.push_back(v);
+        }
+        return vec;
     }
 };
